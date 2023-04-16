c4af2e09f0c758f6e0e7199122aeb34af3e8da75
MyWaterBottle <bobotam1500@gmail.com>
2023-04-16 00:21:08 -0400

    Add .env file with Firebase configuration values

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+require('dotenv').config;
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCEUY5XKTB9iJU46MhQNPAdwM-NaBC0UBE",
-  authDomain: "mytransitmtl-b49dd.firebaseapp.com",
-  projectId: "mytransitmtl-b49dd",
-  storageBucket: "mytransitmtl-b49dd.appspot.com",
-  messagingSenderId: "480265413009",
-  appId: "1:480265413009:web:3d1530a71ffa2cbf79fa30"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
